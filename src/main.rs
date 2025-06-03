b03966587b3f3581b7e12b39a8980bdb332a4582
ibro <sameoldlab@proton.me>
2025-06-03 17:54:27 +0100

    relmed

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,70 @@
-use gtk::{glib, Application, ApplicationWindow};
-use gtk::{prelude::*, Button};
+use gtk::{prelude::*, Orientation};
 use gtk4_layer_shell::{Edge, Layer, LayerShell};
+use relm4::{RelmApp, SimpleComponent};
 
 const APP_ID: &str = "supply.same.Slabs";
 
-fn build_ui(app: &Application) {
-    let window = ApplicationWindow::new(app);
-    window.init_layer_shell();
-    window.set_layer(Layer::Top);
-    window.auto_exclusive_zone_enable();
-
-    for anchor in [Edge::Left, Edge::Right, Edge::Top] {
-        window.set_anchor(anchor, true);
-    }
-
-    window.present();
+#[derive(Debug)]
+enum Messages {}
+struct Model {
+    workspace: String,
+    window: String,
+    time: String,
 }
 
-fn main() -> glib::ExitCode {
-    let app = Application::builder().application_id(APP_ID).build();
-    app.connect_activate(build_ui);
-    app.run()
+#[relm4::component]
+impl SimpleComponent for Model {
+    type Init = u8;
+    type Input = Messages;
+    type Output = ();
+
+    view! {
+        gtk::ApplicationWindow {
+            init_layer_shell: (),
+            set_layer: Layer::Top,
+            auto_exclusive_zone_enable: (),
+            set_anchor: (Edge::Left, true),
+            set_anchor: (Edge::Right, true),
+            set_anchor: (Edge::Top, true),
+
+            gtk::Box {
+                set_orientation: Orientation::Horizontal,
+                gtk::Label {
+                    #[watch]
+                    set_label: &model.workspace
+                },
+                gtk::Label {
+                    #[watch]
+                    set_label: &model.window
+                },
+                gtk::Label {
+                    #[watch]
+                    set_label: &model.time
+                }
+            }
+        }
+    }
+
+    fn init(
+        _init: Self::Init,
+        root: Self::Root,
+        _sender: relm4::ComponentSender<Self>,
+    ) -> relm4::ComponentParts<Self> {
+        let model = Model {
+            workspace: "".to_string(),
+            window: "".to_string(),
+            time: "".to_string(),
+        };
+        let widgets = view_output!();
+        relm4::ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, message: Self::Input, _sender: relm4::ComponentSender<Self>) {
+        match message {}
+    }
+}
+
+fn main() {
+    let app = RelmApp::new(APP_ID);
+    app.run::<Model>(0);
 }
